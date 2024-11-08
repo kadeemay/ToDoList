4b6d6286ad074382470ecd4d9cf7268fbede6d71
kadeemay <kadeemayw@gmail.com>
2024-11-07 18:54:22 -0800

    basic version

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  ToDoList
 //
-//  Created by Kadee Wheeler on 11/3/24.
+//  Created by Kadee Wheeler on 10/3/24.
 //
 
 import SwiftUI
--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -2,7 +2,7 @@
 //  ToDoListApp.swift
 //  ToDoList
 //
-//  Created by Kadee Wheeler on 11/3/24.
+//  Created by Kadee Wheeler on 10/3/24.
 //
 
 import SwiftUI
